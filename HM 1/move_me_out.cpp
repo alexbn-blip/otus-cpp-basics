37391f0318325c1a365043d3f68207127b5c6a29
alexbn-blip <vip.sterling@icloud.com>
2026-05-29 20:43:36 +0300

    Add files via upload

---
+++ HM 1/move_me_out.cpp
@@ -0,0 +1,7 @@
+#include "move_me_out.h"
+#include <iostream>
+
+void todo_move_me_out() {
+    std::cout << "This function has been moved out!" << std::endl;
+    // Здесь оригинальная реализация функции
+}
