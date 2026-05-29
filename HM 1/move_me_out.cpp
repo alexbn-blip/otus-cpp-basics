ff501e5aadac6702cab93ac7775247f4445b9e8b
alexbn-blip <vip.sterling@icloud.com>
2026-05-29 20:46:28 +0300

    Delete HM 1 directory

--- HM 1/move_me_out.cpp
+++
@@ -1,7 +0,0 @@
-#include "move_me_out.h"
-#include <iostream>
-
-void todo_move_me_out() {
-    std::cout << "This function has been moved out!" << std::endl;
-    // Здесь оригинальная реализация функции
-}
